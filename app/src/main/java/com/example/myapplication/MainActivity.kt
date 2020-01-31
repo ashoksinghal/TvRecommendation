e051979da93a8788ffdd85decf8693891211194b
raghav sharma <raghav.sharma@telekom-digital.com>
2020-01-31 07:54:59 +0530

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val type = object : TypeToken<ArrayList<Channel>>() {}.type
+
+
+        
+
+        GlobalScope.async(Dispatchers.IO) {
+
+            val string = URL("http://10.178.23.94:1337/channels/recommend/1").readText()
+            val result = Gson().fromJson<List<Channel>>(
+                string,
+                type
+            )
+            GlobalScope.launch(Dispatchers.Main) {
+                recyclerview.adapter = ChannelAdapter(result)
+            }
+        }
+    }
+}
